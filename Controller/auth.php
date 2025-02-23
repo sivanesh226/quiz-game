2d7472a0b1b49b82cf96f8cbf6dea9c23b9229d5
Sivanesh <smartsiva226@gmail.com>
2025-02-23 14:20:16 +0530

    Auth token check

--- Controller/auth.php
+++ Controller/auth.php
@@ -2,18 +2,33 @@
 require '../Model/db.php';
 require 'jwt_helper.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
-$email = $data['email'];
-$password = $data['password'];
-$stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? AND password = SHA2(?,224)" );
-$stmt->execute([$email,$password]);
-$user = $stmt->fetch();
+$action = $_GET['action'] ?? '';
+if ($action == 'login') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $email = $data['email'];
+    $password = $data['password'];
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? AND password = SHA2(?,224)" );
+    $stmt->execute([$email,$password]);
+    $user = $stmt->fetch();
 
-if ($user) {
-    // password_verify($password, $user['password'])
-    $token = generate_jwt(['email' => $email, ]);
-    echo json_encode(['status'=> true, 'result'=> [  'token' => $token, 'user_id' => $user['id'], 'name' => $user['name'], 'email' => $user['email'], 'role'=>$user['role']] ]);
-} else {
-    echo json_encode(['error' => 'Invalid credentials']);
+    if ($user) {
+        // password_verify($password, $user['password'])
+        $token = generate_jwt(['email' => $email, ]);
+        echo json_encode(['status'=> true, 'result'=> ['token' => $token, 'user_id' => $user['id'], 'name' => $user['name'], 'email' => $user['email'], 'role'=>$user['role']] ]);
+    } else {
+        echo json_encode(['error' => 'Invalid credentials']);
+    }
+} elseif ($action == 'auto_login') {
+    $email=token_validate();
+    if($email) {
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?" );
+        $stmt->execute([$email]);
+        $user = $stmt->fetch();
+        if ($user) {
+            echo json_encode(['status'=> true, 'result'=> ['token' => $token, 'user_id' => $user['id'], 'name' => $user['name'], 'email' => $user['email'], 'role'=>$user['role']] ]);
+        } else {
+            echo json_encode(['error' => 'Invalid Token']);
+        }
+    }
 }
 ?>
