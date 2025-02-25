027816206b51602eb8bf3fff3d7f10c2777beda0
Sivanesh <smartsiva226@gmail.com>
2025-02-26 02:29:51 +0530

    Bug fix in limit

--- Controller/admin_user.php
+++ Controller/admin_user.php
@@ -15,7 +15,7 @@ if ($decoded_token && isset($decoded_token['email'])) {
     if (!empty($search_key)) {
         $stmt = $pdo->prepare("SELECT * FROM users WHERE name LIKE ? OR email LIKE ? LIMIT ?");
         $search_term = "%$search_key%";
-        $stmt->bindParam(1, $limit, PDO::PARAM_INT);
+        $stmt->bindParam(3, $limit, PDO::PARAM_INT);
         $stmt->execute([$search_term, $search_term]);
     } else {
         $stmt = $pdo->prepare("SELECT id, name, email, role, (password IS NOT NULL AND password != '') AS has_password FROM users LIMIT ?");
