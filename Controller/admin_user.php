7ae268cd91aad61fb6c4ee453b644a1ad0349813
Sivanesh <smartsiva226@gmail.com>
2025-02-26 02:42:13 +0530

    Password side user admin bug fix

--- Controller/admin_user.php
+++ Controller/admin_user.php
@@ -27,8 +27,10 @@ require 'jwt_helper.php';
     if ($users) {
         // Convert 'has_password' to boolean and rename to 'password'
         foreach ($users as &$user) {
-            $user['password'] = (bool) $user['has_password']; // Convert to true/false
-            unset($user['has_password']); // Remove the temporary field
+            if(isset($user['has_password'])) {
+                $user['password'] = (bool) $user['has_password']; // Convert to true/false
+                unset($user['has_password']); // Remove the temporary field
+            }
         }
         echo json_encode(["status" => true, "result" => $users]);
     } else {
